4d26527515bdf82e62efb740a4bdc1607cf13388
Ciana <lfalcon@fi.uba.ar>
2026-03-22 03:53:25 -0300

    Add files via upload

---
+++ TP0-ENUNCIADO-master/src/split.h
@@ -0,0 +1,23 @@
+#ifndef SPLIT_H
+#define SPLIT_H
+
+struct vector {
+	unsigned int cantidad;
+	char **palabras;
+};
+
+/**
+ * Divide un texto en palabras utilizando un separador específico.
+ *
+ * Por ejemplo, si el texto es "Hola;1;2;3;mundo" y el separador es ';',
+ * la función devolverá un vector con las palabras ["Hola", "1", "2", "3",
+ * "mundo"].
+ */
+struct vector *split(char *texto, char separador);
+
+/**
+ * Libera la memoria utilizada por el vector de palabras.
+ */
+void vector_destruir(struct vector *v);
+
+#endif
