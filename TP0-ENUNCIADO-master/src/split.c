9d01896d960448874de9568a2147c1fc54c8dc16
Ciana <lfalcon@fi.uba.ar>
2026-03-22 01:34:24 -0300

    update script

--- TP0-ENUNCIADO-master/src/split.c
+++ TP0-ENUNCIADO-master/src/split.c
@@ -1,60 +1,66 @@
 #include "split.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define CAPACIDAD_INICIAL 10
-
 struct vector *split(char *texto, char separador) {
-  if (texto == NULL) return NULL;
+    int i = 0, j = 0;
+    char vector_auxiliar[100];
 
-  struct vector *v = malloc(sizeof(struct vector));
-  if (!v) return NULL;
+    if (texto == NULL) return NULL;
 
-  size_t capacidad = CAPACIDAD_INICIAL;
-  v->palabras = malloc(capacidad * sizeof(char *));
-  if (!v->palabras) {
-    free(v);
-    return NULL;
-  }
-  v->cantidad = 0;
+    struct vector *v = malloc(sizeof(struct vector));
+    if (!v) return NULL;
 
-  size_t i = 0;
-  size_t inicio = 0;
+    v->palabras = malloc(capacidad, sizeof(char *));
+    v->cantidad = 0;
 
-  while (1) {
-    if (texto[i] == separador || texto[i] == '\0') {
-      size_t largo_palabra = i - inicio;
+    while (texto[i] != '\0') {
+        if (texto[i] != separador) {
+            vector_auxiliar[j] = texto[i];
+            j++;
+        } else {
+            buffer[j] = '\0';
 
-      if (v->cantidad >= capacidad) {
+            if (v->cantidad >= capacidad) {
+                capacidad *= 2;
+                char **temp = realloc(v->palabras, capacidad * sizeof(char *));
+                if (!temp) return v;
+                v->palabras = tmp;
+            }
+
+            v->palabras[v->cantidad] = malloc((j + 1) * sizeof(char));
+            strcpy(v->palabras[v->cantidad], vector_auxiliar);
+            v->cantidad++;
+            j = 0;
+        }
+        i++;
+    }
+
+    if (v->cantidad >= capacidad) {
         capacidad *= 2;
         char **temp = realloc(v->palabras, capacidad * sizeof(char *));
         if (!temp) return v;
-        v->palabras = temp;
-      }
-
-      v->palabras[v->cantidad] = malloc(largo_palabra + 1);
-      if (v->palabras[v->cantidad]) {
-        memcpy(v->palabras[v->cantidad], &texto[inicio], largo_palabra);
-        v->palabras[v->cantidad][largo_palabra] = '\0';
-        v->cantidad++;
-      }
-
-      if (texto[i] == '\0') break;
-      inicio = i + 1;
+        v->palabras = tmp;
     }
-    i++;
-  }
 
-  return v;
+    v->palabras[v->cantidad] = malloc((j + 1) * sizeof(char));
+    strcpy(v->palabras[v->cantidad], vector_auxiliar);
+    v->cantidad++;
+
+    return v;
 }
 
 void vector_destruir(struct vector *v) {
-  if (!v) return;
-  for (size_t i = 0; i < v->cantidad; i++) {
-    free(v->palabras[i]);
-  }
-  free(v->palabras);
-  free(v);
+    if (!v) return;
+
+    for (int i = 0; i < v->cantidad; i++) {
+        free(v->palabras[i]);
+    }
+
+    free(v->palabras);
+    free(v);
 }
 
 /*int main() {
